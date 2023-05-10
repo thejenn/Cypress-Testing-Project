9a248b63ba7cd568158243312f208c4b0850b3dc
jennharmless <jharmless@riis.com>
2023-05-10 13:59:38 -0400

    more cucumber

--- cypress/integration/examples/BDD/ecommerce.feature
+++ cypress/integration/examples/BDD/ecommerce.feature
@@ -4,6 +4,14 @@ Feature: End to end Ecommerce validation
 
     Scenario: Ecommerce products delivery
     Given I open ECommerce Page
-    When I add items to card
-    And Validate the total prices
+    When I add items to cart
+    Then Validate the total prices
     Then select the country submit and verify Thankyou     
+
+    Scenario: Filling the form to shop
+    Given I open ECommerce Page
+    When I fill the form details
+    |name | gender |
+    |bobz | Male   |
+    Then validate the forms behaviour
+    Then select the Shop Page
--- cypress/integration/examples/BDD/ecommerce/eCommStepDef.js
+++ cypress/integration/examples/BDD/ecommerce/eCommStepDef.js
@@ -1,6 +1,84 @@
-import { Given,When,Then } from "@badeball/cypress-cucumber-preprocessor";
+import HomePage from "../../../pageObjects/HomePage";
+import ProductPage from "../../../pageObjects/ProductsPage";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+const homePage = new HomePage()
+const productPage = new ProductPage()
 
 Given('I open ECommerce Page', ()=>
 {
     cy.visit(Cypress.env('url')+"/angularpractice/")
 })
+
+When('I add items to cart', function ()
+{
+    homePage.getShopTab().click()
+    this.data.productName.forEach(function (element) {
+
+        cy.selectProduct(element)
+      });
+      productPage.checkOutButton().click()
+})
+
+Then('Validate the total prices', ()=>
+{
+    var sum = 0
+
+    cy.get('tr td:nth-child(4) strong').each(($el, index, $list) => {
+
+      const amount = $el.text()
+      var res = amount.split(" ")
+      res = res[1].trim()
+      sum = Number(sum) + Number(res)
+
+    }).then(function () {
+      cy.log(sum)
+    })
+    cy.get('h3 strong').then(function (element) {
+      const amount = element.text()
+      var res = amount.split(" ")
+      var total = res[1].trim()
+      expect(Number(total)).to.equal(sum)
+    })
+
+})
+
+Then('select the country submit and verify Thankyou', ()=>
+{
+    cy.contains('Checkout').click()
+    cy.get('#country').type('India')
+    cy.get('.suggestions > ul > li > a', { timeout: 10000 }).should('be.visible')
+    cy.get('#checkbox2').click({ force: true })
+    cy.get('input[type="submit"]').click() 
+    cy.get('.alert').then(function (element) {
+
+      const actualText = element.text()
+      expect(actualText.includes("Success")).to.be.true
+
+    })
+
+})
+
+When('I fill the form details', function(dataTable)
+{
+    homePage.getEditBox().type(dataTable.rawTable[1][0])
+    homePage.getGender().select(dataTable.rawTable[1][1])
+
+    
+})
+
+Then('validate the forms behaviour', function()
+{
+    homePage.getTwoWayDataBinding().should('have.value', this.data.name)
+    homePage.getEditBox().should('have.attr', 'minlength', '2')
+})
+
+Then('select the Shop Page', ()=>
+{
+    homePage.getShopTab().click()
+})
+
+
+
+
+
