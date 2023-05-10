51b04223f3ad0e04e1af69bc87df1bfcfb1afc89
jennharmless <jharmless@riis.com>
2023-05-10 10:56:41 -0400

    commit after cucumber addition

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,32 @@
+
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-   
-}
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
 
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 module.exports = defineConfig({
 
+  defaultCommandTimeout: 6000,
   env: {
-    url: 'https://rahulshettyacademy.com',    
+    url: "https://rahulshettyacademy.com",
   },
+  retries: {
+    runMode: 1,
+
+  },
+  projectId: "nodpcq",
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents,
     specPattern: 'cypress/integration/examples/*.js'
+
   },
 });
